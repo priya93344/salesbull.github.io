fcf9dfc113f49deef76613d2a4c9aac9c67e7ffd
priya93344 <pk4979509@gmail.com>
2023-04-08 19:08:58 +0530

    update

---
+++ wp-content/plugins/countdown-timer-ultimate/includes/admin/settings/solution-features/pro-features-tab.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Admin Class
+ *
+ * Handles the Admin side functionality of plugin
+ *
+ * @package Popup Anything on click
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+} ?>
+
+<div id="wpcdt_themes_tabs" class="wpcdt-vtab-cnt wpcdt_themes_tabs wpcdt-clearfix">
+	<!-- Start - Countdown - Features -->
+	<div class="wpcdt-sf-features-section">
+		<div class="wpcdt-sf-center wpcdt-features-ttl">
+			<h1 class="popupaoc-heading">Powerful Pro Features, Simplified</h1>
+		</div>
+		<div class="wpcdt-sf-welcome-wrap wpcdt-sf-center">
+			<div class="wpcdt-sf-features-box-wrap">
+				<ul class="wpcdt-sf-features-box-grid">
+					<li>
+						<div class="wpcdt-popup-icon"><img src="<?php echo WPCDT_URL; ?>/assets/images/popup-icon/circle-count-down.png" /></div>Circle Countdown Timer
+					</li>
+					<li>
+						<div class="wpcdt-popup-icon"><img src="<?php echo WPCDT_URL; ?>/assets/images/popup-icon/flip-count-down.png" /></div>Flip Countdown Timer
+					</li>
+					<li>
+						<div class="wpcdt-popup-icon"><img src="<?php echo WPCDT_URL; ?>/assets/images/popup-icon/simple-clock-2.png" /></div>Simple Clock Timer
+					</li>
+					<li>
+						<div class="wpcdt-popup-icon"><img src="<?php echo WPCDT_URL; ?>/assets/images/popup-icon/circle-count-down.png" /></div>Evergreen Timer
+					</li>
+					<li>
+						<div class="wpcdt-popup-icon"><img src="<?php echo WPCDT_URL; ?>/assets/images/popup-icon/circle-count-down.png" /></div>Recurring Timer
+					</li>
+					<li>
+						<div class="wpcdt-popup-icon"><img src="<?php echo WPCDT_URL; ?>/assets/images/popup-icon/circle-count-down.png" /></div>Schedule Timer
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- End - Countdown - Features -->
+
+	<div class="wpcdt-deal-offer-wrap">
+		<div class="wpcdt-deal-offer"> 
+			<div class="wpcdt-inn-deal-offer">
+				<h3 class="wpcdt-inn-deal-hedding"><span>Buy Countdown Timer Ultimate Pro</span> today and unlock all the powerful features.</h3>
+				<h4 class="wpcdt-inn-deal-sub-hedding"><span style="color:red;">Extra Bonus: </span>Users will get <span>15% off</span> on the regular price using this coupon code.</h4>
+			</div>
+			<div class="wpcdt-inn-deal-offer-btn">
+				<div class="wpcdt-inn-deal-code"><span>EPS15</span></div>
+				<a href="<?php echo WPCDT_PLUGIN_BUNDLE_LINK; ?>" target="_blank" class="wpcdt-sf-btn wpcdt-sf-btn-orange"><span class="dashicons dashicons-cart"></span> Get Essential Bundle Now</a>
+			</div>
+		</div>
+	</div>
+</div>
